0f47d003922a5cc5a20101bf7f0369cd4b5cc54b
Ayesh Vininda <ayeshgk@gmail.com>
2024-04-23 23:58:17 +0530

    change redirect url

--- src/providers/auth.provider.js
+++ src/providers/auth.provider.js
@@ -2,8 +2,10 @@ import React from "react";
 import { AuthProvider } from "@asgardeo/auth-react";
 
 const config = {
-     signInRedirectURL: "http://localhost:3000/chats",
-     signOutRedirectURL: "http://localhost:3000/",
+    //  signInRedirectURL: "http://localhost:3000/chats",
+      //  signOutRedirectURL: "http://localhost:3000/",
+     signOutRedirectURL: "https://37566d18-8521-4284-abb8-1ab716473b8c.e1-us-east-azure.choreoapps.dev/",
+    signInRedirectURL: "https://37566d18-8521-4284-abb8-1ab716473b8c.e1-us-east-azure.choreoapps.dev/chats",
      clientID: "CMFfmVLc4aGP_1EV8U8mcBaAmF4a",
      baseUrl: "https://api.asgardeo.io/t/cybersynced",
      scope: [ "openid","profile" ]
