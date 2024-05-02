80144ea3ab4262ed2281060b5ffa47dfc0935c4a
Ayesh Vininda <ayeshgk@gmail.com>
2024-05-02 17:58:02 +0530

    update 2

--- src/providers/auth.provider.js
+++ src/providers/auth.provider.js
@@ -2,11 +2,15 @@ import React from "react";
 import { AuthProvider } from "@asgardeo/auth-react";
 
 const config = {
-      signInRedirectURL: "http://localhost:3000/chats",
-        signOutRedirectURL: "http://localhost:3000/",
+    //   signInRedirectURL: "http://localhost:3000/chats",
+    //     signOutRedirectURL: "http://localhost:3000/",
+    // developemnt 
 //     signOutRedirectURL: "https://37566d18-8521-4284-abb8-1ab716473b8c.e1-us-east-azure.choreoapps.dev/",
 //    signInRedirectURL: "https://37566d18-8521-4284-abb8-1ab716473b8c.e1-us-east-azure.choreoapps.dev/chats",
-     clientID: "CMFfmVLc4aGP_1EV8U8mcBaAmF4a",
+    // production 
+    signOutRedirectURL: "https://009171b2-6d99-4524-ac73-de4e43f6744e.e1-us-east-azure.choreoapps.dev/",
+    signInRedirectURL: "https://009171b2-6d99-4524-ac73-de4e43f6744e.e1-us-east-azure.choreoapps.dev/chats", 
+    clientID: "CMFfmVLc4aGP_1EV8U8mcBaAmF4a",
      baseUrl: "https://api.asgardeo.io/t/cybersynced",
      scope: [ "openid","profile" ]
 };
